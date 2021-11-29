e5bc83a6a767b247afce0cf55258b2a6946e68ac
Juan5757 <juanjo1998_x@hotmail.com>
2021-11-29 18:47:13 -0400

    iniciando el edit profile lider user

--- Automation/features/4EditProfile.feature
+++ Automation/features/4EditProfile.feature
@@ -1,15 +1,17 @@
-Feature: As voluntario user
+Feature: As "<typeOfUser>" user
          I want to edit my profile
          so that I can update the information of my profile
 
 
-Scenario: Edit the name of the profile        
-  Given the voluntario user is logged with the email and password on Start Americas Together homepage
-  When the voluntario user click on "Perfil" option
+Scenario Outline:Edit the name of the profile        
+  Given the "<typeOfUser>" user is logged with the email and password on Start Americas Together homepage
+  When the "<typeOfUser>" user click on "Perfil" option
   And click on EDITAR PERFIL
-  And the voluntario user will update his name to Juan
+  And the "<typeOfUser>" user will update his name to Juan
   And click on GUARDAR
-  Then the voluntario user can see his name updated on the Perfil information with "Juan"
+  Then the "<typeOfUser>" user can see his name updated on the Perfil information with "Juan"
 
-
-
+Examples: 
+|typeOfUser|
+|voluntario|
+|lider     |
--- Automation/features/step_definitions/04EditProfileSteps.rb
+++ Automation/features/step_definitions/04EditProfileSteps.rb
@@ -2,18 +2,22 @@
 
 
 
-Given(/^the voluntario user is logged with the email and password on Start Americas Together homepage$/) do 
+Given(/^the "([^"]*)" user is logged with the email and password on Start Americas Together homepage$/) do |typeOfUser|
     visit 'https://testing-start.web.app/'
     buttonLogin_xpath='//*[@id="root"]/header/div[1]/button'
     find(:xpath, buttonLogin_xpath).click
-    fill_in 'email', :with => ENV['VOLUNTARIO_USER']
+    if(typeOfUser== "voluntario")
+        fill_in 'email', :with => ENV['VOLUNTARIO_USER']
+    elsif(typeOfUser== "lider")
+        fill_in 'email', :with => ENV['LIDER_USER']
+    end
     fill_in 'password', :with => ENV['PSW']
     buttonIniciarSesion_xpath='//*[@id="root"]/div/div[2]/div/form/div[3]/button[1]'
     find(:xpath, buttonIniciarSesion_xpath).click
     sleep 5
   end
   
-  When(/^the voluntario user click on "([^"]*)" option$/) do |perfilButton|
+  When(/^the "([^"]*)" user click on "([^"]*)" option$/) do |typeOfUser, perfilButton|
     buttonPerfil_xpath='//*[@id="root"]/header/div[2]/div/button[4]'
     find(:xpath, buttonPerfil_xpath).click
   end
@@ -23,7 +27,7 @@ Given(/^the voluntario user is logged with the email and password on Start Ameri
     find(:xpath, buttonEditarPerfil_xpath).click
   end
   
-  When(/^the voluntario user will update his name to Juan$/) do 
+  When(/^the "([^"]*)" user will update his name to Juan$/) do |typeOfUser|
     fill_in 'Nombre', :with => 'Juan'
   end
 
@@ -32,9 +36,9 @@ Given(/^the voluntario user is logged with the email and password on Start Ameri
     find(:xpath, buttonGuardar_xpath).click
   end
   
-  Then(/^the voluntario user can see his name updated on the Perfil information with "([^"]*)"$/) do |name|
+  Then(/^the "([^"]*)" user can see his name updated on the Perfil information with "([^"]*)"$/) do |typeOfUser,name|
     userName = find(:xpath,'//*[@id="simple-tabpanel-0"]/div/span/div/div[1]/div[2]/div/div[1]/div/p[1]').text
-    textName = "Nombre: "+name  
+    textName = "Nombre: "+name
     if (userName != textName)
         raise "Validation for user name: Failed"    
         puts "Expected: "+textName
