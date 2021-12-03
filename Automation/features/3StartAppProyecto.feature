79c24195e846693b244e93131014ad896b74d4fe
Allen Vargas <allenbfox@gmail.com>
2021-12-03 02:58:06 -0400

    Steps Create a project

--- Automation/features/3StartAppProyecto.feature
+++ Automation/features/3StartAppProyecto.feature
@@ -3,18 +3,19 @@ Feature: Create a project on Start Americas Together
   I want to create a project on the platform
   So that I can create a new project for the company and volunteers
 
-Scenario: Create a complete new project in StartApp
+@maximize
+Scenario Outline: Create a complete new project in StartApp
   Given the leader user is logged with the email and password on Start Americas Together homepage
   And I press the Proyectos button
-  When I press the "CREAR PROYECTO" button
-  And I enter required fields as shown below
-	  |Titulo:  	        | Salvar Perritos                            |
-    |Descripcion: 	    | Ayudar a los perro callejeros de la calle  |
-    |Objetivo: 	        | Tener el 50% de los perros en un refugio   |
-    |Lider:   	        | Allen                                      |
+  When I press the button to create a new proyect "CREAR PROYECTO"
+  And I enter the required fields for a new project as show below
+	  |Titulo:  	        | Salvar Perritos                             |
+    |Descripcion: 	    | Ayudar a los perros callejeros de la calle  |
+    |Objetivo: 	        | Tener el 50% de los perros en un refugio    |
+    |Lider:   	        | Lider-A                                     |
   And click on CREAR button
   Then a card is created with the project according to the following data
     |Proyecto: 	        | Salvar Perritos                            |
     |Objetivo: 	        | Tener el 50% de los perros en un refugio   |
-    |Descripcion: 	    | Ayudar a los perro callejeros de la calle  |
-    |Lider: 	          | Allen                                      |
+    |Descripcion: 	    | Ayudar a los perros callejeros de la calle |
+    |Lider: 	          | Lider-A                                    |
--- Automation/features/step_definitions/3StartAppSteps.rb
+++ Automation/features/step_definitions/3StartAppSteps.rb
@@ -1,23 +1,91 @@
 #You can implement step definitions for undefined steps with these snippets:
 
 Given('the leader user is logged with the email and password on Start Americas Together homepage') do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit 'https://testing-start.web.app/'
+  buttoLogin_xpath = '/html/body/div/header/div[1]/button'
+  find(:xpath, buttoLogin_xpath).click
+  fill_in 'email', :with => ENV['LIDER_USER']
+  fill_in 'password', :with => ENV['PSW']
+  buttonIniciarSesion_xpath='//*[@id="root"]/div/div[2]/div/form/div[3]/button[1]'
+  find(:xpath, buttonIniciarSesion_xpath).click
+  sleep(2)
 end
 
 Given('I press the Proyectos button') do
-  pending # Write code here that turns the phrase above into concrete actions
+  buttonProyectos = find(:xpath, '//*[@id="root"]/header/div[2]/div/button[2]')
+  buttonProyectos.click
+  sleep(1)
 end
 
-When('I enter required fields as shown below') do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+When('I press the button to create a new proyect {string}') do |string|
+  buttonCrearProyectos = find(:xpath, '//*[@id="root"]/div/div[1]/button')
+  buttonCrearProyectos.click
+  sleep(1)
+end
+
+When('I enter the required fields for a new project as show below') do |table|
+  data = table.rows_hash
+  data.each_pair do |key, value|
+    case key
+    when "Titulo:"
+      find(:xpath, '//*[@id="gen-form"]/form/div[2]/div[1]/input').set(value)
+    when "Descripcion:"
+      find(:xpath, '//*[@id="gen-form"]/form/div[2]/div[2]/input').set(value)
+    when "Objetivo:"
+      find(:xpath, '//*[@id="gen-form"]/form/div[2]/div[3]/input').set(value) 
+    when "Lider:"
+      find(:xpath, '//*[@id="gen-form"]/form/div[2]/div[4]/input').set(value)        
+    end  
+  end
+  sleep(2)
 end
 
 When('click on CREAR button') do
-  pending # Write code here that turns the phrase above into concrete actions
+  buttonCrear = find(:xpath, '//*[@id="gen-form"]/form/div[2]/div[5]/input')
+  bottonCancelar = find(:xpath, '//*[@id="gen-form"]/button')
+  buttonCrear.click
+  #bottonCancelar.click
+  sleep(1)
 end
 
 Then('a card is created with the project according to the following data') do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+  print("=======================")
+  x= '//*[@id="root"]/div/div[2]/div'
+  count = page.all(:xpath, x).length
+  print(count)
+  print("=======================")
+  dataRes = table.rows_hash
+  dataRes.each_pair do |key, value|
+    case key
+    when "Proyecto:"
+      #//*[@id="root"]/div/div[2]/div[6]/div[2]/p[1]
+      textProyecto = find(:xpath, '//*[@id="root"]/div/div[2]/div[1]/div[2]/p[1]')
+      txt = textProyecto.text.split("Proyecto: ").join("")
+      if txt != value
+        raise "The message should be "+ value
+      end
+      
+    when "Objetivo:"
+      textObjetivo = find(:xpath, '//*[@id="root"]/div/div[2]/div[1]/div[2]/p[2]')
+      txt = textObjetivo.text.split("Objetivo: ").join("")
+      if txt != value
+        raise "The message should be "+ value	
+      end
+
+    when "Descripcion:"
+      textDescripcion = find(:xpath, '//*[@id="root"]/div/div[2]/div[1]/div[2]/p[3]')
+      txt = textDescripcion.text.split("Descripción: ").join("")
+      if txt != value
+        raise "The message should be "+ value	
+      end
+
+    when "Lider:"
+      textLider = find(:xpath, '//*[@id="root"]/div/div[2]/div[1]/div[2]/p[4]')
+      txt = textLider.text.split("Lider: ").join("")
+      if txt != value
+        raise "The message should be "+ value	
+      end
+    end  
+  end
+  sleep(2)
 end
